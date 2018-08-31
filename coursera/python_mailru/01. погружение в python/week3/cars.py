ca6af6f2bb94c13864eff40610a4aabfac7bf2fe
brain4power <yotakrasnodar@yandex.ru>
2018-08-31 19:04:55 +0300

    импорт файла с данными для обработки

--- coursera/python_mailru/01. погружение в python/week3/cars.py
+++ coursera/python_mailru/01. погружение в python/week3/cars.py
@@ -1,8 +1,13 @@
+import os
+import csv
+
+
 class CarBase:
     def __init__(self, brand, photo_file_name, carrying):
         pass
     def get_photo_file_ext(self):
         pass
+
 
 class Car(CarBase):
     def __init__(self, brand, photo_file_name, carrying, passenger_seats_count):
@@ -16,6 +21,7 @@ class Truck(CarBase):
     def get_body_volume(self):
         pass
 
+
 class SpecMachine(CarBase):
     def __init__(self, brand, photo_file_name, carrying, extra):
         pass
@@ -23,4 +29,24 @@ class SpecMachine(CarBase):
 
 def get_car_list(csv_filename):
     car_list = []
+    with open(csv_filename) as csv_fd:
+        reader = csv.reader(csv_fd, delimiter=';')
+        next(reader)  # пропускаем заголовок
+        for row in reader:
+
+            print(row)
     return car_list
+
+
+car_turple = {'car', 'truck', 'SpecMachine'}
+
+get_car_list('coursera_week3_cars.csv')
+
+if 'car' in car_turple:
+    print('tyta')
+
+
+# Далее необходимо реализовать функцию, на вход которой подается имя файла в формате csv.
+# Файл содержит данные аналогичные строкам из таблицы. Вам необходимо прочитать этот файл
+# построчно при помощи модуля стандартной библиотеки csv. Затем проанализировать строки и
+# создать список нужных объектов с автомобилями и специальной техникой. Функция должна возвращать список объектов.
