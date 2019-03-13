38d6f9d7a35809bd5dbc533ecaf6a58dfc3d7f9b
brain4power <yotakrasnodar@yandex.ru>
2019-03-13 19:07:34 +0300

    доделал файл с магическими методами

--- coursera/python_mailru/01. погружение в python/week4/magic.py
+++ coursera/python_mailru/01. погружение в python/week4/magic.py
@@ -16,10 +16,10 @@ class File:
         with open(self.path, 'w', encoding='UTF-8') as fd:
             fd.write(input_text)
 
-    # def read(self):
-    #     with open(self.path, 'r', encoding='UTF-8') as fd:
-    #         fd.seek(0)
-    #         return fd.read()
+    def read(self):
+        with open(self.path, 'r', encoding='UTF-8') as fd:
+            fd.seek(0)
+            return fd.read()
 
     def __add__(self, obj1):
         storage_path = os.path.join(tempfile.gettempdir(), 'summary.txt')
@@ -28,17 +28,26 @@ class File:
                 self_file.seek(0)
                 with open(obj1.path, 'r') as obj1_file:
                     obj1_file.seek(0)
-                    new_obj_file.write(self_file.read() + obj1_file.read())
+                    new_obj_file.write(self_file.read() + '\n' + obj1_file.read())
         return File(storage_path)
 
     def __str__(self):
         return self.path
 
+    def __iter__(self):
+        return open(self.path, 'r', encoding='UTF-8')
 
-# first = File('first.txt')
-# second = File('second.txt')
-# first.write('1')
-# second.write('2')
-# new_obj = first + second
-# #print(first.read() + second.read())
-# print(new_obj.read())
+
+if __name__ == "__main__":
+    first = File('first.txt')
+    second = File('second.txt')
+    #first.write('1111\n22222')
+    second.write('2')
+    new_obj = first + second
+    #print(first.read() + second.read())
+    #print(new_obj.read())
+    for line in first:
+        print('line=', line)
+    # with open('first.txt', 'r') as te:
+    #     print(te.readline())
+    #     print('-------')
