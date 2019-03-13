41b8705bd34cc021305dd59deb80a8f094f1fdc9
brain4power <yotakrasnodar@yandex.ru>
2019-03-13 15:50:19 +0300

    start week 4

--- coursera/python_mailru/01. погружение в python/week3/cars.py
+++ coursera/python_mailru/01. погружение в python/week3/cars.py
@@ -4,58 +4,47 @@ import csv
 
 class CarBase:
     def __init__(self, brand, photo_file_name, carrying):
-        self.brand = brand
-        self.photo_file_name = photo_file_name
-        self.carrying = carrying
+        pass
 
-    def get_photo_file_ext(self):
-        return os.path.splitext(self.photo_file_name)[1]
+    def get_photo_file_ext(self, photo_file_name):
+        #os.path.splitext
+        pass
 
 
 class Car(CarBase):
     def __init__(self, brand, photo_file_name, carrying, passenger_seats_count):
-        super().__init__(brand, photo_file_name, carrying)
-        self.passenger_seats_count = passenger_seats_count
+        pass
 
 
 class Truck(CarBase):
     def __init__(self, brand, photo_file_name, carrying, body_whl):
-        super().__init__(brand, photo_file_name, carrying)
-        if body_whl:
-            self.body_length, self.body_width, self.body_height = map(float, body_whl.split('x'))
-        else:
-            self.body_whl = self.body_length = self.body_width = self.body_height = 0.0
+        pass
 
     def get_body_volume(self):
-        return self.body_length * self.body_width * self.body_height
+        pass
 
 
 class SpecMachine(CarBase):
     def __init__(self, brand, photo_file_name, carrying, extra):
-        super().__init__(brand, photo_file_name, carrying)
-        self.extra = extra
+        pass
 
 
 def get_car_list(csv_filename):
     car_list = []
-    with open(csv_filename, encoding='UTF-8') as csv_fd:
+    with open(csv_filename) as csv_fd:
         reader = csv.reader(csv_fd, delimiter=';')
         next(reader)  # пропускаем заголовок
         for row in reader:
-           try:
-                if row[0] == 'car' and '.' in row[3] and len(row[1]):
-                    car_list.append(Car(row[1], row[3], float(row[5]), int(row[2])))
-                elif row[0] == 'truck' and '.' in row[3] and len(row[1]):
-                    car_list.append(Truck(row[1], row[3], float(row[5]), row[4]))
-                elif row[0] == 'spec_machine' and '.' in row[3] and len(row[1]):
-                    car_list.append(SpecMachine(row[1], row[3], float(row[5]), row[6]))
-           except:
-               pass
+            if len(row) and row[0] == 'car' and row[1] and row[2] and row[3] and row[5]:
+                car_list.append(Car)
+            elif len(row) and row[0] == 'truck' and row[1] and row[3] and row[5]:
+                car_list.append(Truck)
+            elif len(row) and row[0] == 'spec_machine' and row[1] and row[3] and row[5] and row[6]:
+                car_list.append(SpecMachine)
     return car_list
 
 
-#print(get_car_list('coursera_week3_cars.csv'))
-# print(get_car_list('coursera_week3_cars.csv')[1].get_body_volume())
+print(get_car_list('coursera_week3_cars.csv'))
 
 # Далее необходимо реализовать функцию, на вход которой подается имя файла в формате csv.
 # Файл содержит данные аналогичные строкам из таблицы. Вам необходимо прочитать этот файл
