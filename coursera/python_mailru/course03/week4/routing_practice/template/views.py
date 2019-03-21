3d60ec4f8ec2f5e47048262ec1ca2e061ac8b5c1
brain4power <yotakrasnodar@yandex.ru>
2019-03-21 15:47:50 +0300

    сделал часть 1 задания по шаблонизации

--- coursera/python_mailru/course03/week4/routing_practice/template/views.py
+++ coursera/python_mailru/course03/week4/routing_practice/template/views.py
@@ -1,10 +1,28 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 
 # Create your views here.
 
 def echo(request):
-    pass
+    response = ''
+    if request.method == 'GET':
+        if request.GET:
+            response += 'get '
+            for each in request.GET:
+                response += f'{each}: {request.GET[each]} '
+
+    if request.method == 'POST':
+        if request.POST:
+            response += 'post '
+            for each in request.POST:
+                response += f'{each}: {request.POST[each]} '
+
+    try:
+        response += f'statement is {request.META["HTTP_X-PRINT-STATEMENT"]}'
+    except KeyError:
+        response += 'statement is empty'
+    return HttpResponse(content=response, status=200)
 
 
 def filters(request):
